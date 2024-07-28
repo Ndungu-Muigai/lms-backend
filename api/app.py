8afc365cd4c402a2a4dda0ffeaa99f63f1b5b3d0
Samuel Muigai <ndungu.muigai01@gmail.com>
2024-07-28 13:53:50 +0300

    Changed the redis URL to the Vercel redis database

--- api/app.py
+++ api/app.py
@@ -20,7 +20,7 @@ app.config["SECRET_KEY"]=os.environ["SECRET_KEY"]
 app.config["SESSION_TYPE"]="redis"
 app.config["SESSION_PERMANENT"]=True
 app.config["SESSION_USER_SIGNER"]=False
-app.config["SESSION_REDIS"]=redis.from_url("redis://subtle-wasp-31289.upstash.io")
+app.config["SESSION_REDIS"]=redis.from_url(os.getenv("KV_URL"))
 app.config["PERMANENT_SESSION_LIFETIME"]=timedelta(minutes=30)
 
 #Configuring the database
