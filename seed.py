2ebe4bde617ab200db21097c9e9d4826eefed8f4
Samuel Muigai <ndungu.muigai01@gmail.com>
2024-08-11 15:26:55 +0300

    Changed the database URL and modified the employees table to include the country and phone number

--- seed.py
+++ seed.py
@@ -1,5 +1,5 @@
-from app import app
-from models import Employee, LeaveApplication, LeaveDays, db, OneTimePassword
+from api.app import app
+from api.models import Employee, LeaveApplication, LeaveDays, db, OneTimePassword
 
 if __name__ == "__main__":
     with app.app_context():
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         OneTimePassword.query.delete()
         db.session.commit()
 
-        new_employee=Employee(first_name="Samuel",last_name="Muigai", gender="Male", email="muigaisam65@gmail.com", username="smuigai", section="Mobikey", department="Logistics", position="Logistics Assistant", role="HR", password="214aaf2c9a8510d948555ee25cb38397")
+        new_employee=Employee(first_name="Samuel",last_name="Muigai", gender="Male", email="muigaisam65@gmail.com", username="smuigai", country="KE", phone="+254707251073", department="Logistics", position="Logistics Assistant", role="HR", password="214aaf2c9a8510d948555ee25cb38397")
         db.session.add(new_employee)
         db.session.commit()
 
