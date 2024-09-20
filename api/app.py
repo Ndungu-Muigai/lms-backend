42d113c43aff221b3be975d2f252c6d5aa3a1059
Samuel Muigai <107753424+Ndungu-Muigai@users.noreply.github.com>
2024-09-20 15:28:52 +0300

    Switching the redis URL to AWS ElastiCache

--- api/app.py
+++ api/app.py
@@ -22,7 +22,7 @@ import io
 app = Flask(__name__)
 
 # Configuring redis
-r=redis.Redis(host="mobikey-lms-rvdpsl.serverless.use1.cache.amazonaws.com", port=6379, ssl=False)
+r=redis.Redis(host="mobikey-lms-rvdpsl.serverless.use1.cache.amazonaws.com", port=6379, ssl=True, socket_timeout=10)
 app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
 app.config["SESSION_TYPE"] = "redis"
 app.config['SESSION_REDIS'] = r
