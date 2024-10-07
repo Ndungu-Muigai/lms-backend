642088c9c30ed44914d2dec709e2cbae955a4048
Samuel Muigai <ndungu.muigai01@gmail.com>
2024-10-07 20:19:51 +0300

    Modified the OTP generation code and the email sent to the user once the OTP is generated

--- Generations/otp.py
+++ Generations/otp.py
@@ -2,19 +2,19 @@ import random
 import string
 
 def get_otp():
-    # Select 4 random numbers between 1 and 9
-    numbers = [random.randint(1, 9) for _ in range(4)]
+    # Select 2 random uppercase letters
+    letters = [random.choice(string.ascii_uppercase) for _ in range(2)]
     
-    # Select 4 random letters between 'a' and 'z'
-    letters = [random.choice(string.ascii_lowercase) for _ in range(4)]
+    # Select 4 random digits between 0 and 9
+    numbers = [random.choice(string.digits) for _ in range(4)]
     
-    # Combine the numbers and letters
-    code = numbers + letters
+    # Combine the letters and numbers
+    code = letters + numbers
     
     # Shuffle the combined list to randomize the order
     random.shuffle(code)
     
     # Convert the list to a string
-    code_string = ''.join(str(item) for item in code)
+    code_string = ''.join(code)
     
-    return code_string.upper()
+    return code_string
