b28f9987f99ba6828f4b1f0178b17d676d3996c7
Samuel Muigai <ndungu.muigai01@gmail.com>
2024-10-08 19:52:48 +0300

    Added David's credentials

--- seed.py
+++ seed.py
@@ -18,12 +18,20 @@ if __name__ == "__main__":
         db.session.add(muigai_leave_days)
         db.session.commit()
 
-        lourdes_wairimu=Employee(first_name="Lourdes", last_name="Wairimu", gender="Female", email="lourdeswairimu@gmail.com", username="lwairimu", country="KE", phone="+254745416760", department="Administration", position="HR Manager", role="HR", password="7ffdd51bc0133c88d240889d7d6a5214")
+        lourdes_wairimu=Employee(first_name="Lourdes", last_name="Wairimu", gender="Female", email="lourdeswairimu@gmail.com", username="lwairimu", country="KE", phone="+254745416760", department="Administration", position="HR Manager", role="HR", password="cf63d8b585dd65eae31466ede6fe65c5", first_login=False)
         db.session.add(lourdes_wairimu)
         db.session.commit()
 
         wairimu_leave_days=LeaveDays(employee_id=lourdes_wairimu.id, normal_leave=21, sick_leave=14, maternity_leave=90, paternity_leave=0)
         db.session.add(wairimu_leave_days)
+        db.session.commit()
+
+        david_tundo=Employee(first_name="David",last_name="Tundo", gender="Male", email="david.tundo@mobikey.co.ke", username="dtundo", country="KE", phone="+254793305103", department="Finance", position="Tax and Admin Manager", role="HOD", password="f30da452351da3baea61fa20ac419010")
+        db.session.add(samuel_muigai)
+        db.session.commit()
+
+        tundo_leave_days=LeaveDays(employee_id=david_tundo.id, normal_leave=21, sick_leave=14, maternity_leave=0, paternity_leave=14)
+        db.session.add(muigai_leave_days)
         db.session.commit()
 
         print("Information added successfully")
