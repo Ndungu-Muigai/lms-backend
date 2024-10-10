fb4fc7540d4facf6263f7571dd41a343c9bd877d
Samuel Muigai <ndungu.muigai01@gmail.com>
2024-10-10 16:49:35 +0300

    Saving the employee's branch in the database

--- api/app.py
+++ api/app.py
@@ -591,7 +591,10 @@ class ApprovedRequests(Resource):
             #Creating a dict of the requests
             leave_requests_dict=LeaveApplicationsSchema(only=("id","employee", "leave_type", "leave_duration","start_date", "end_date", "total_days","file_attachment","status")).dump(leave_requests, many=True)
 
-        return make_response(jsonify(leave_requests_dict), 200)
+        return make_response(jsonify({
+            "success" : "Fetched successfully",
+            "leave_data": leave_requests_dict
+        }), 200)
 
 api.add_resource(ApprovedRequests, "/approved-requests")
 
